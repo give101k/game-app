d48afc8d9f0d8f99ad746e580b75735d33dc44b1
give101k <give101k@gmail.com>
2017-03-12 21:50:33 -0400

    changed some of the styles

--- javas.js
+++ javas.js
@@ -1,30 +1,35 @@
-$(document).ready(function(){
-    $("#b1").click(function(){
-        $("#Rules").toggle();
-        $("#home_page").toggle();
-    });
-
-
-     $("#b2").click(function(){
-        $("#Game").toggle();
-        $("#home_page").toggle();
-    });
-
-     $("#b3").click(function() {
-     	$("#Leaderboard").toggle();
-     	$("#home_page").toggle();
-     });
-
-     $("#b4").click(function() {
-     	$("#game_over").toggle();
-     	$("#home_page").toggle();
-     });
-
-    $('.home').click(function(){
+function hideall() {
         $("#game_over").hide();
         $("#Leaderboard").hide();
         $("#Game").hide();
         $("#Rules").hide();
+        $("#home_page").hide();	
+}
+
+$(document).ready(function(){
+    $("#b1").click(function(){
+        hideall();
+        $("#Rules").show();
+    });
+
+
+     $("#b2").click(function(){
+        hideall();
+        $("#Game").show();
+    });
+
+     $("#b3").click(function() {
+     	hideall();
+     	$("#Leaderboard").show();
+     });
+
+     $("#b4").click(function() {
+     	hideall();
+     	$("game_over").show();
+     });
+
+    $('.home').click(function(){
+    	hideall();
         $("#home_page").show();
     });
 });
