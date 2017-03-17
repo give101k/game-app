ba2857337c242908248818dc23884a87f4059b24
give101k <give101k@gmail.com>
2017-03-17 00:47:03 -0400

    stage 2

--- javas.js
+++ javas.js
@@ -4,6 +4,7 @@ function hideall() {
         $("#Game").hide();
         $("#Rules").hide();
         $("#home_page").hide();	
+        $("#quit").hide();
 }
 
 $(document).ready(function(){
@@ -25,11 +26,42 @@ $(document).ready(function(){
 
      $("#b4").click(function() {
      	hideall();
-     	$("game_over").show();
+     	$("#game_over").show();
      });
 
     $('.home').click(function(){
     	hideall();
         $("#home_page").show();
     });
+
+    $('#play-again').click(function(){
+        hideall();
+        $("#Game").show();
+    });
+
+    $('#exit').click(function(){
+        hideall();
+        $("#home_page").show();
+    });
+
+    $('#exit_button').click(function(){
+        hideall();
+        $('#Game').show();
+        $('#quit').show();
+    });
+
+    $('#no').click(function(){
+        hideall();
+        $('#Game').show();
+    });
+
+    $('#yes').click(function(){
+        hideall();
+        $('#home_page').show();
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  hideall();
+  $("#home_page").show();
 });
